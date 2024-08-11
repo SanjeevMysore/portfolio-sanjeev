00aac4fdb44e148e4ad261e075b78326cfcbb609
Sanjeev Mysore <sanjeev.m@krypc.com>
2024-08-11 19:18:19 +0530

    projects badge responsiveness

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -35,9 +35,16 @@ const Projects = () => {
                     className='w-full max-w-xl lg:w-3/4'>
                     <h6 className='mb-2 font-semibold'>{projects?.title}</h6>
                     <p className='mb-4 text-neutral-400'>{projects?.description}</p>
-                    {projects?.technologies.map((tech, index)=>(
-                        <span key={index} className='mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>{tech}</span>
+                    <div className="flex flex-wrap">
+                    {projects?.technologies.map((tech, index) => (
+                        <span 
+                        key={index} 
+                        className='mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'
+                        >
+                        {tech}
+                        </span>
                     ))}
+                    </div>
                     </motion.div>
                 </div>
             ))}
